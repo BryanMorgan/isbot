5d2825b4ed63efb442539cabf9dacb071645c9dd
Bryan Morgan <bryanmorgan@gmail.com>
2022-03-23 16:32:07 -0700

    Add methods to `append` and `remove` bot patterns.
    Update unit tests and docs to validate `append` and `remove`. Add `include-default-bots` feature to allow default bots to be included or excluded from the compiled library.

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Rust library to detect bots using a user-agent string.
 - Focused on speed, simplicity, and ensuring real devices and browsers don't get falsely identified as bots
 - Tested on over *12k* bot user agents and *180k* browser user agents - updated bot and browser lists are downloaded as part of the integration test suite
 - Includes a default collection of 300+ known bot user-agent regular expressions at compile time
+- Allows user-agent patterns to be manually added and removed at runtime
 
 ## Usage
 
@@ -32,6 +33,56 @@ assert_eq!(bots.is_bot("Googlebot-Image/1.0"), true);
 assert_eq!(bots.is_bot("Opera/9.60 (Windows NT 6.0; U; en) Presto/2.1.1"), false);
 ```
 
+## Customizing
+Bot user-agent patterns can be customized by adding or removing patterns, using the `append` and `remove` methods.
+
+### Add bot pattern
+To add new bot patterns, use `append` to specify an array of regular expression patterns. For example:
+
+```rust
+let mut bots = isbot::Bots::default();
+
+assert_eq!(bots.is_bot("Mozilla/5.0 (CustomNewTestB0T /1.2)"), false);
+
+bots.append(&[r"CustomNewTestB0T\s/\d\.\d"]);
+
+assert_eq!(bots.is_bot("Mozilla/5.0 (CustomNewTestB0T /1.2)"), true);
+```
+
+### Remove bots
+To remove bot patterns, use `remove` and specify an array of existing patterns to remove. For example, to remove the Chrome Lighthouse user-agent pattern to indicate it is not a bot:
+```rust
+let mut bots = isbot::Bots::default();
+
+bots.remove(&["Chrome-Lighthouse"]);
+
+assert_eq!(bots.is_bot("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 Chrome-Lighthouse"), false);
+```
+
+### Custom Bot list
+The default user-agent regular expression patterns are managed in the [bot_patterns.txt](./src/bot_patterns.txt) file.
+
+If you don't want to use the default bot patterns you can supply your own list. Since the default bot patterns are automatically added to the library at compile time you should first disable the default feature. The `include-default-bots` feature is enabled by default so the patterns defined in `bot_patterns.txt` are included in the library at compile time.
+
+You can exclude the patterns by disabling the default features and then including your own bot regular expressions. To do that set `default-features` to false in your `Cargo.toml` dependency definition. For example:
+
+```toml
+[dependencies]
+isbot = { version = "0.1.0", default-features = false }
+```
+
+And then use `Bots::new()` to supply a newline delimited list of regular expressions. For example:
+
+```rust
+use isbot::Bots;
+
+let custom_user_agent_patterns = r#"
+^Googlebot-Image/
+bingpreview/"#;
+
+let bots = Bots::new(custom_user_agent_patterns);
+assert_eq!(bots.is_bot("Googlebot-Image/1.0"), true);
+```
 
 ## Testing
 Some of the test fixture data is download from multiple sources to ensure the latest user-agents are validated. 
--- src/lib.rs
+++ src/lib.rs
@@ -28,12 +28,27 @@ use std::{collections::HashSet, fmt::Debug};
 ///
 /// // By default Chrome Lighthouse is considered a bot
 /// assert_eq!(bots.is_bot("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 Chrome-Lighthouse"), true);
+/// // Remove the Chrome Lighthouse regular expression pattern to indicate it is not a bot
+/// bots.remove(&["Chrome-Lighthouse"]);
+/// assert_eq!(bots.is_bot("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 Chrome-Lighthouse"), false);
+///
+/// // Append a new custom bot user-agent regular expression
+/// assert_eq!(bots.is_bot("Mozilla/5.0 (CustomNewTestB0T /1.2)"), false);
+/// bots.append(&[r"CustomNewTestB0T\s/\d\.\d"]);
+/// assert_eq!(bots.is_bot("Mozilla/5.0 (CustomNewTestB0T /1.2)"), true);
 ///
 pub struct Bots {
+    user_agent_patterns: HashSet<String>,
     combined_user_agent_regex: Regex,
 }
 
+/// Load default bot user-agent regular expressions from a local file, unless the feature is disabled
+#[cfg(feature = "include-default-bots")]
 const BOT_REGEX_LIST: &str = include_str!("bot_patterns.txt");
+
+/// Do not load any default user-agent strings into the compiled library if feature is not enabled
+#[cfg(not(feature = "include-default-bots"))]
+const BOT_REGEX_LIST: &str = "";
 
 impl Default for Bots {
     /// Constructs a new instance with default user-agent patterns.
@@ -75,6 +90,7 @@ impl Bots {
         let user_agent_patterns = Bots::parse_lines(&bot_entries.to_ascii_lowercase());
         let combined_user_agent_regex = Bots::to_regex(&user_agent_patterns);
         Bots {
+            user_agent_patterns,
             combined_user_agent_regex,
         }
     }
@@ -96,6 +112,59 @@ impl Bots {
     pub fn is_bot(&self, user_agent: &str) -> bool {
         self.combined_user_agent_regex
             .is_match(&user_agent.to_ascii_lowercase())
+    }
+
+    /// Appends bot user-agent regular expressions patterns.
+    ///
+    /// Duplicates are ignored.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use isbot::Bots;
+    ///
+    /// let mut bots = Bots::default();
+    /// assert_eq!(bots.is_bot("Mozilla/5.0 (CustomNewTestB0T /1.2)"), false);
+    /// bots.append(&[r"CustomNewTestB0T\s/\d\.\d"]);
+    /// assert_eq!(bots.is_bot("Mozilla/5.0 (CustomNewTestB0T /1.2)"), true);
+    ///
+    /// let new_bot_patterns = vec!["GoogleMetaverse", "^Special/"];
+    /// bots.append(&new_bot_patterns);
+    /// assert_eq!(bots.is_bot("Mozilla/5.0 (GoogleMetaverse/1.0)"), true);
+    pub fn append(&mut self, bots: &[&str]) {
+        for bot in bots {
+            self.user_agent_patterns.insert(bot.to_ascii_lowercase());
+        }
+        self.update_regex()
+    }
+
+    /// Removes bot user-agent regular expressions.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use isbot::Bots;
+    ///
+    /// let mut bots = Bots::default();
+    ///
+    ///
+    /// assert_eq!(bots.is_bot("Chrome-Lighthouse"), true);
+    /// bots.remove(&["Chrome-Lighthouse"]);
+    /// assert_eq!(bots.is_bot("Chrome-Lighthouse"), false);
+    ///
+    /// let bot_patterns_to_remove = vec!["bingpreview/", "Google Favicon"];
+    /// bots.remove(&bot_patterns_to_remove);
+    /// assert_eq!(bots.is_bot("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534+ (KHTML, like Gecko) BingPreview/1.0b"), false);
+    /// assert_eq!(bots.is_bot("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36 Google Favicon"), false);
+    pub fn remove(&mut self, bots: &[&str]) {
+        for bot in bots {
+            self.user_agent_patterns.remove(&bot.to_ascii_lowercase());
+        }
+        self.update_regex()
+    }
+
+    fn update_regex(&mut self) {
+        self.combined_user_agent_regex = Bots::to_regex(&self.user_agent_patterns)
     }
 
     fn parse_lines(bot_regex_entries: &str) -> HashSet<String> {
@@ -195,5 +264,61 @@ mod tests {
         assert_eq!(bots.is_bot("M"), false);
         assert_eq!(bots.is_bot("Me"), true);
         assert_eq!(bots.is_bot("Googlebot"), false);
+    }
+    #[test]
+    fn add_pattern() {
+        let mut bots = Bots::default();
+        assert_eq!(bots.is_bot("Mozilla/5.0 (FancyNewTestB0T /1.2)"), false);
+        bots.append(&[r"FancyNewTestB0T\s/\d\.\d"]);
+        assert_eq!(bots.is_bot("Mozilla/5.0 (FancyNewTestB0T /1.2)"), true);
+    }
+
+    #[test]
+    fn add_multiple_patterns() {
+        let mut bots = Bots::default();
+        assert_eq!(bots.is_bot("Mozilla/5.0 (FancyNewTestB0T /1.2)"), false);
+        assert_eq!(bots.is_bot("Special/1.0"), false);
+        assert_eq!(bots.is_bot("GoogleMetaverse/2.1 (experimental)"), false);
+
+        let new_bot_patterns = vec!["FancyNewTestB0T", "^GoogleMetaverse", "^Special/"];
+        bots.append(&new_bot_patterns);
+
+        assert_eq!(bots.is_bot("Mozilla/5.0 (FancyNewTestB0T /1.2)"), true);
+        assert_eq!(bots.is_bot("Special/1.0"), true);
+        assert_eq!(bots.is_bot("GoogleMetaverse/2.1 (experimental)"), true);
+    }
+
+    #[test]
+    fn remove_pattern() {
+        let mut bots = Bots::default();
+        assert_eq!(bots.is_bot("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 Chrome-Lighthouse"), true);
+        bots.remove(&["Chrome-Lighthouse"]);
+        assert_eq!(bots.is_bot("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 Chrome-Lighthouse"), false);
+        assert_eq!(bots.is_bot("Chrome-Lighthouse"), false);
+        assert_eq!(bots.is_bot("Mozilla/5.0 (Windows NT 10.0; Win64; x64) adbeat.com/policy AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"), true);
+    }
+
+    #[test]
+    fn remove_multiple_patterns() {
+        let mut bots = Bots::default();
+        assert_eq!(bots.is_bot("Datadog Agent/5.10.1"), true);
+        assert_eq!(bots.is_bot("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 Chrome-Lighthouse"), true);
+        assert_eq!(bots.is_bot("Mozilla/5.0 (Java) outbrain"), true);
+        assert_eq!(
+            bots.is_bot("Mozilla/5.0 (compatible; Google-Site-Verification/1.0)"),
+            true
+        );
+
+        let bot_patterns_to_remove =
+            vec!["datadog agent", "Chrome-Lighthouse", "outbrain", "google-"];
+        bots.remove(&bot_patterns_to_remove);
+
+        assert_eq!(bots.is_bot("Datadog Agent/5.10.1"), false);
+        assert_eq!(bots.is_bot("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36 Chrome-Lighthouse"), false);
+        assert_eq!(bots.is_bot("Mozilla/5.0 (Java) outbrain"), false);
+        assert_eq!(
+            bots.is_bot("Mozilla/5.0 (compatible; Google-Site-Verification/1.0)"),
+            false
+        );
     }
 }
